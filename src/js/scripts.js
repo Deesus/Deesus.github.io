fdbda5a2bfc1b4817855c3845b21a728677f982f
Dee Reddy <d.reddy@yandex.com>
2016-03-27 04:58:52 -0400

    refactor: Update menu styles.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,13 +3,16 @@ $(document).ready(function() {
     var menuShown       = false;
     var $menuSelection  = $('.triple-bar, #drop-down-menu > a');
     var $dropMenu       = $('#drop-down-menu ul');
+    var $menuContainer  = $('#drop-down-menu');
 
     $menuSelection.click(function() {
         if(menuShown) {
             $dropMenu.removeClass('show-menu');
+            $menuContainer.removeClass('fill-bg');
         }
         else {
             $dropMenu.addClass('show-menu');
+            $menuContainer.addClass('fill-bg');
         }
         menuShown = !menuShown;
     });
