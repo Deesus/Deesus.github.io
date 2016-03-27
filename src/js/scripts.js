ee71665d366a7494cee2ca3573e1cf5cded60b1c
Dee Reddy <d.reddy@yandex.com>
2016-03-27 18:08:39 -0400

    refactor: Finalize adaptive menu.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,19 +1,41 @@
 'use strict';
 $(document).ready(function() {
+    /*************************
+     * Initialize variables, functions:
+     *************************/
     var menuShown       = false;
-    var $menuSelection  = $('.triple-bar, #drop-down-menu > a');
-    var $dropMenu       = $('#drop-down-menu ul');
     var $menuContainer  = $('#drop-down-menu');
+    var $menuSelection  = $menuContainer.find('a');
+    var $dropMenu       = $menuContainer.children('ul');
+    function showMenu() {
+        $dropMenu.addClass('show-menu');
+        $menuContainer.addClass('fill-bg');
+        menuShown = true;
+    }
+    function hideMenu() {
+        $dropMenu.removeClass('show-menu');
+        $menuContainer.removeClass('fill-bg');
+        menuShown = false;
+    }
 
+    /*************************
+     * Event handlers:
+     *************************/
+    // If 'triple-bar' logo is clicked, display/hide menu:
     $menuSelection.click(function() {
-        if(menuShown) {
-            $dropMenu.removeClass('show-menu');
-            $menuContainer.removeClass('fill-bg');
+        if(!menuShown) {
+            showMenu();
         }
         else {
-            $dropMenu.addClass('show-menu');
-            $menuContainer.addClass('fill-bg');
+            hideMenu();
         }
-        menuShown = !menuShown;
+    });
+
+    // Corner case: if menu is shown but user resizes to larger width, we must hide menu:
+    // TODO: Derive a non-JS solution for hiding menu icon upon browser resize?
+    $(window).resize(function() {
+        if(window.innerWidth > 540){
+            hideMenu();
+        }
     });
 });
