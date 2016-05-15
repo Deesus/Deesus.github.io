3d21eb9f2f761131030ceb96333f16568ff3ec1b
Dee Reddy <d.reddy@yandex.com>
2016-05-15 18:14:24 -0400

    chore: Build and fix links.

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -4,36 +4,29 @@ $(document).ready(function() {
      * Initialize variables, functions:
      *************************/
     var menuShown       = false;
-    var $menuLinks      = $('#drop-down-menu').find('a');
-    var $entirePage     = $('.entire-page');
+    var $links          = $('.hamburger-icon, #hamburger-menu a, .ion-ios-close-empty');
+    var $menu           = $('#hamburger-menu');
     function showMenu() {
-        $entirePage.removeClass('hidden');
-        $menuLinks.removeClass('hidden');
+        // TODO: change to 'toggle' method:
+        $menu.removeClass('menu-hidden');
+        $menu.addClass('menu-visible');
         menuShown = true;
     }
     function hideMenu() {
-        $entirePage.addClass('hidden');
-        $menuLinks.addClass('hidden');
+        $menu.removeClass('menu-visible');
+        $menu.addClass('menu-hidden');
         menuShown = false;
     }
 
     /*************************
      * Event handlers:
      *************************/
-    // If 'triple-bar' logo is clicked, display/hide menu:
-    $menuLinks.click(function() {
+    // If 'hamburger' logo is clicked, display/hide menu:
+    $links.click(function(event) {
         if(!menuShown) {
             showMenu();
         }
         else {
-            hideMenu();
-        }
-    });
-
-    // Corner case: if menu is shown but user resizes to larger width, we must hide menu:
-    // TODO: Derive a non-JS solution for hiding menu icon upon browser resize?
-    $(window).resize(function() {
-        if(window.innerWidth > 540){
             hideMenu();
         }
     });
