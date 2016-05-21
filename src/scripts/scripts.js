c9c86628089498f530cbb20634f2fc2454dcd781
Dee Reddy <d.reddy@yandex.com>
2016-05-21 17:32:50 -0400

    format: Clean scripts.

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,26 +1,29 @@
 'use strict';
 $(document).ready(function() {
-    /*************************
-     * Initialize variables, functions:
-     *************************/
+    /**
+     * slide-in side menu (responsive menu):
+     * 
+     * TODO:
+     *  corner cases:   - if viewport/browser is resized [larger] then menu should disappear
+     *                  - if user clicks outside menu, menu should close
+     *                  - lock scroll bar
+     */
+    // initialize variables, functions:
     var menuShown       = false;
     var $links          = $('.hamburger-icon, #hamburger-menu a, .ion-ios-close-empty');
     var $menu           = $('#hamburger-menu');
     function showMenu() {
         // TODO: change to 'toggle' method:
-        $menu.removeClass('menu-hidden');
-        $menu.addClass('menu-visible');
+        $menu.removeClass('menu-hidden')
+             .addClass('menu-visible');
         menuShown = true;
     }
     function hideMenu() {
-        $menu.removeClass('menu-visible');
-        $menu.addClass('menu-hidden');
+        $menu.removeClass('menu-visible')
+             .addClass('menu-hidden');
         menuShown = false;
     }
-
-    /*************************
-     * Event handlers:
-     *************************/
+    // Event handlers:
     // If 'hamburger' logo is clicked, display/hide menu:
     $links.click(function(event) {
         if(!menuShown) {
