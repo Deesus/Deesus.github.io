86211db24b463b807e830c6d8fca9e3ad22e49b8
Dee Reddy <d.reddy@yandex.com>
2016-10-24 04:06:11 -0400

    add maps

---
+++ src/scripts/mapping.js
@@ -0,0 +1,78 @@
+'use strict';
+window.MAPPING  = (function () {
+    /// -----------------------------------------------------
+    /// Variables:
+    /// -----------------------------------------------------
+
+    var hereMap,
+        googleMap,
+        X_COORD,
+        Y_COORD,
+        zoomLevel,
+        circleStyle;
+
+    /// Initial coordinate location (Chicago):
+    X_COORD     = 41.89961;
+    Y_COORD     = -87.67243;
+    zoomLevel   = 13;
+
+    /// If we want the same circle size/styles, we can just define them upfront:
+    /// N.b. we could also have created a circle object (c.f. Google Maps example below)
+    circleStyle = {
+        stroke:     false,      /// disables perimeter
+        fillColor:  '#006AFF',  /// color of sector (i.e. entire area)
+        fillOpacity: 0.3,
+        radius:      800        /// N.b. if we use `.circle()` method, this is in meters, if we use `.circleMaker()` method, this is is pixels.
+                                /// The difference is, when the radius is set by meters, the circle 'shrinks' if we zoom out and grows when we zoom in.
+                                /// When the radius is in pixels, zooming doesn't affect the circle size on the map.
+    };
+
+    /// -----------------------------------------------------
+    /// Initialize map
+    /// -----------------------------------------------------
+
+    /// Initialize map and set coordinates + zoom level:
+    hereMap = L.map('here-map') /// Set id selector of map
+               .setView([X_COORD, Y_COORD], zoomLevel);
+
+    /// Set Leaflet URL template c.f. <https://developer.here.com/news/201602172>
+    L.tileLayer(
+        'http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/{scheme}/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}',
+        {
+            attribution:'all hail Dee | <a href="http://developer.here.com">HERE</a>',     /// N.b. we can turn off Leaflet attributions <http://leafletjs.com/reference.html#control-attribution>
+            subdomains: '1234',
+            base:       'base',
+            type:       'maptile',
+            scheme:     'pedestrian.day',
+            app_id:     'LYBuzdgM3acdB6lQ3di3',       /// add your app id here
+            app_code:   '6KqoxqRAamfrWU2GrlK4DQ',     /// add your app code here
+            mapID:      'newest',
+            maxZoom:    20,
+            language:   'eng',
+            format:     'png8',
+            size:       '256'
+        }
+    ).addTo(hereMap);                                 /// add map object to HTML
+
+    /// -----------------------------------------------------
+    /// Draw on map:
+    /// -----------------------------------------------------
+
+    /// Create a new pin (Leaflet refers to pins as "marker") (in this case, at the same location as map initialization coordinates):
+    L.marker([X_COORD, Y_COORD], zoomLevel
+    ).addTo(hereMap);
+    
+    /// Create a Circle (once again, on the same coordinates as before):
+    /// N.b. Alternatively, we could have used `.circleMaker()` <http://leafletjs.com/reference.html#circlemarker>
+    L.circle(
+        [X_COORD, Y_COORD],     /// set center
+        circleStyle             /// we passed our pre-defined circle styles
+    ).addTo(hereMap);
+
+
+    /// -----------------------------------------------------
+    /// Now for Google Maps:
+    /// -----------------------------------------------------
+
+})();
+
