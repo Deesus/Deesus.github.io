fe77aa341248488c7ee4fa3a49c15383cf6aef41
Dee Reddy <d.reddy@yandex.com>
2016-12-03 19:10:33 -0500

    Rollback: Revert maps blog.

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,39 +1,36 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', (function() {
+$(document).ready(function() {
     /**
      * slide-in side menu (responsive menu):
-     *
+     * 
      * TODO:
      *  corner cases:   - if viewport/browser is resized [larger] then menu should disappear
      *                  - if user clicks outside menu, menu should close
      *                  - lock scroll bar
      */
     // initialize variables, functions:
-    var menuShown   = false;
-    var $links      = document.querySelectorAll('.hamburger-icon, #hamburger-menu a, .ion-ios-close-empty');
-    var $menu       = document.getElementById('hamburger-menu');  // TODO: replace with query selector?
-
+    var menuShown       = false;
+    var $links          = $('.hamburger-icon, #hamburger-menu a, .ion-ios-close-empty');
+    var $menu           = $('#hamburger-menu');
     function showMenu() {
         // TODO: change to 'toggle' method:
-        $menu.classList.remove('menu-hidden');
-        $menu.classList.add('menu-visible');
+        $menu.removeClass('menu-hidden')
+             .addClass('menu-visible');
         menuShown = true;
     }
     function hideMenu() {
-        $menu.classList.remove('menu-visible');
-        $menu.classList.add('menu-hidden');
+        $menu.removeClass('menu-visible')
+             .addClass('menu-hidden');
         menuShown = false;
     }
     // Event handlers:
     // If 'hamburger' logo is clicked, display/hide menu:
-    for(var i = 0; i < $links.length; i++){
-        $links[i].addEventListener('click', function (event) {
-            if(!menuShown) {
-                showMenu();
-            }
-            else {
-                hideMenu();
-            }
-        })
-    }
-})());
+    $links.click(function(event) {
+        if(!menuShown) {
+            showMenu();
+        }
+        else {
+            hideMenu();
+        }
+    });
+});
